7f429e2e4775151a75ce4c6fa0a3449a397ef5dc
NateNate60 <nathanlim2004@gmail.com>
2021-06-15 13:07:44 -0700

    More changes

--- wallet.py
+++ wallet.py
@@ -7,57 +7,117 @@ import bitcash
 import hashlib
 import time
 import random
+import config
+import web3
+import lit
+import praw
 from decimal import Decimal
+
+w3 = web3.Web3(web3.Web3.HTTPProvider('https://kovan.infura.io/v3/79f3325b70d147d0beda556e812b41d4'))
+r = praw.Reddit(username = config.username, password = config.password, client_id = config.client_id, client_secret = config.client_secret, user_agent = "Nate'sEscrowBot")
+
+
 
 #Class representing an escrow transaction
 class Escrow :
-    def __init__(self, id) -> None:
+    def __init__(self, coin: str) -> None:
         #escrow id
         #in reality just the hash of the current time
         h = hashlib.sha1()
         h.update((str(time.time()) + str(random.random())).encode('utf-8'))
         self.id = h.hexdigest()
 
+        #state of the escrow. 0 = waiting deposit, 1 = funded & held, 2 = released, -1 = refunded
+        self.state = 0
         
         #which coin the escrow is holding (ex. "btc")
-        self.coin = ""
+        self.coin = coin
 
         #sender's username (ex. "NateNate60")
         self.sender = ""
 
-        #sender's crypto address
-        self.senderaddr = ""
-
         #recipient's username (ex. "NateNate60")
         self.recipient = ""
 
-        #recipient's crypto address
-        self.recipientaddr = ""
-
         #value of the escrow in crypto (ex. 0.0001)
-        self.value = ""
+        self.value = Decimal(0.)
 
         #whether or not the escrow is considered funded
         self.funded = False
 
         #the WIF private key for the address holding the escrowed funds
-        self.privkey = ""
+        self.privkey = None
+        if (self.coin == 'btc') :
+            if (not config.testnet) :
+                k = bit.Key()
+                self.privkey = k.to_wif()
+            else :
+                k = bit.PrivateKeyTestnet()
+                self.privkey = k.to_wif()
+        elif (self.coin == 'bch') :
+            if (not config.testnet) :
+                k = bitcash.key()
+                self.privkey = h.to_wif()
+            else :
+                k = bitcash.PrivateKeyTestnet()
+                self.privkey = k.to_wif()
+        elif (self.coin == 'eth') :
+            pass
 
-    def release (self, amount: str = "") :
+
+    def pay (self, addr: str, feerate: int = 0, ) :
         """
-        Release the escrow to the recipient. amount is the amount to release, defaulting to the entire value.
-        If the escrow is partially released, the value is reduced accordingly
+        Send the funds to addr with a given feerate
         """
-        if (Decimal(amount) > Decimal(self.value)) :
-            return 1
+        txid = ""
+        if (self.coin == 'btc') :
+            k = bit.Key(self.privkey)
+            if (feerate == 0) :
+                txid = k.send([(addr, float(self.value - Decimal(config.escrowfee['btc'])), 'btc')], leftover=config.leftover['btc'])
+            else :
+                txid = k.send([(addr, float(self.value - Decimal(config.escrowfee['btc'])), 'btc')], leftover=config.leftover['btc'], fee=feerate)
+        elif (self.coin == 'bch') :
+            k = bitcash.Key(self.privkey)
+            if (feerate == 0) :
+                txid = k.send([(addr, float(self.value - Decimal(config.escrowfee['bch'])), 'bch')], leftover=config.leftover['bch'])
+            else :
+                txid = k.send([(addr, float(self.value - Decimal(config.escrowfee['bch'])), 'bch')], leftover=config.leftover['bch'], fee=feerate)
+        elif (self.coin == 'ltc') :
+            k = lit.Key(self.privkey)
+            if (feerate == 0) :
+                txid = k.send([(addr, float(self.value - Decimal(config.escrowfee['ltc'])), 'ltc')], leftover=config.leftover['ltc'])
+            else :
+                txid = k.send([(addr, float(self.value - Decimal(config.escrowfee['ltc'])), 'ltc')], leftover=config.leftover['ltc'], fee=feerate)
+        return txid
+
+    def __notifyavailable (self, sender: bool = False) :
+        """
+        Notify the user of the availability of funds. sender is whether the funds are released to the sender, defaulting to False,
+        which releases to recipient
+        """
+        if (sender) :
+            r.redditor(self.sender).message("Funds available", str(self.value) + " " + self.coin.upper() + " was released to you. You may withdraw the funds using `!withdraw [address]`." +
+                                            " If you wish to specify a custom feerate, you may do so by using `!withdraw [address] [feerate]`.\n\n" +
+                                            "    ESCROW VALUE: " + str(self.value) + " " + self.coin.upper() + '\n' +
+                                            "    ESCROW FEE: " + str(Decimal(config.escrowfeee[self.coin])) + " " + self.coin.upper() + '\n' +
+                                            "    TOTAL AVAILABLE (before network fees): " + str(self.value - Decimal(config.escrowfee['ltc'])))
         else :
-            self.value = str(Decimal(self.value) - Decimal(amount))
-        
-
+            r.redditor(self.recipient).message("Funds available", str(self.value) + " " + self.coin.upper() + " was released to you. You may withdraw the funds using `!withdraw [address]`." +
+                                            " If you wish to specify a custom feerate, you may do so by using `!withdraw [address] [feerate]`.\n\n" +
+                                            "    ESCROW VALUE: " + str(self.value) + " " + self.coin.upper() + '\n' +
+                                            "    ESCROW FEE: " + str(Decimal(config.escrowfeee[self.coin])) + " " + self.coin.upper() + '\n' +
+                                            "    TOTAL AVAILABLE (before network fees): " + str(self.value - Decimal(config.escrowfee['ltc'])))
     
-    def refund (amount: str = "") :
+    def refund (self) :
         """
-        Refund the money held in escrow to the sender. amount is the amount to release, defautling to the entire value.
-        If the escrow is partially refunded, the value is reduced accordingly
+        Mark the escrow as refunded. The sender will be able to withdraw their funds.
         """
-        pass
+        self.state = -1
+        self.__notifyavailable(True)
+
+    def release (self) :
+        """
+        Mark the escrow as released. The recipient will be able to withdraw their funds.config
+        """
+        self.state = 2
+        self.__notifyavailable()
