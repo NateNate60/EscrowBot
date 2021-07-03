0aa48893b9aebb84d06f310b7c07b0fe468158fa
NateNate60 <nathanlim2004@gmail.com>
2021-07-03 17:28:32 +0000

    Make escrowid option when w/d'ing and joining

--- reddit.py
+++ reddit.py
@@ -1,9 +1,10 @@
+from prawcore import exceptions
 import crypto
 import config
 import praw
 import database
 from decimal import Decimal
-from prawcore.exceptions import NotFound
+import prawcore
 
 r = praw.Reddit(username = config.username, password = config.password, client_id = config.client_id, client_secret = config.client_secret, user_agent = "Nate'sEscrowBot")
 
@@ -50,26 +51,40 @@ def checkinbox(r: praw.Reddit, db: database.Database) -> list :
                 message.reply("Invalid syntax. Please see [this page](https://www.reddit.com/r/Cash4Cash/wiki/index/escrow) for help." + config.signature)
         #Join an escrow transaction as the recipient
         elif ("!join" in b.lower()) :
-            if (len(b.split(' ')) != 2) :
+            escrow = None
+            if (isinstance(message, praw.models.Message) and len(b.split(' ')) == 1) :
+                try :
+                    b = r.inbox.message(message.parent_id[3:]).body.lower()
+                    if ("has invited you to join the escrow with id" not in b) :
+                        raise ValueError
+                    b = b.split('c4cid')[1].split('\n')[0]
+                    escrow = db.lookup('c4cid' + b)
+                    print('Looking up c4cid' + b)
+                except (prawcore.exceptions.Forbidden, ValueError, TypeError) :
+                    message.reply("The message you replied to isn't an invitation to join an escrow. Please try again." + config.signature)
+                    continue
+            elif (len(b.split(' ')) != 2) :
                 message.reply("Invalid syntax. The correct syntax is `!join [escrow ID]`. Escrow IDs begin with \"c4cid\"." + config.signature)
                 continue
-            if ("c4cid" in b.lower().split(' ')[1]) :
-                escrow = db.lookup(b.lower().split(' ')[1])
-                if (escrow == None) :
-                    message.reply("This escrow transaction does not exist." + config.signature)
-                if (message.author.name.lower() == escrow.recipient) :
-                    if (escrow.state == 0) :
-                        escrow.state += 1
-                        escrow.askpayment()
-                        db.add(escrow)
-                        elist.append(escrow)
-                        continue
-                    else :
-                        message.reply("This escrow transaction cannot be joined." + config.signature)
-                        continue
-                else :
-                    message.reply("You are not the intended recipient to this transaction." + config.signature)
+            else :
+                if ("c4cid" in b.lower().split(' ')[1]) :
+                    escrow = db.lookup(b.lower().split(' ')[1])
+            if (escrow == None) :
+                message.reply("This escrow transaction does not exist." + config.signature)
+                continue
+            if (message.author.name.lower() == escrow.recipient) :
+                if (escrow.state == 0) :
+                    escrow.state += 1
+                    escrow.askpayment()
+                    db.add(escrow)
+                    elist.append(escrow)
                     continue
+                else :
+                    message.reply("This escrow transaction cannot be joined." + config.signature)
+                    continue
+            else :
+                message.reply("You are not the intended recipient to this transaction." + config.signature)
+                continue
 
         #Release the escrow to the recipient
         elif ("!release" in b.lower()) :
@@ -125,6 +140,17 @@ def checkinbox(r: praw.Reddit, db: database.Database) -> list :
         #Withdraw funds from an escrow
         elif ("!withdraw" in b.lower()) :
             m = b.split(' ')
+            if ('c4cid' not in m[1]) :
+                try :
+                    p = r.inbox.message(message.parent_id[3:]).body.lower()
+                    if ("was released to you from the escrow with id" not in p) :
+                        raise TypeError
+                    p = p.split('c4cid')[1].split(' ')[0]
+                    m.insert(1, 'c4cid' + p)
+                except (TypeError, prawcore.exceptions.Forbidden) :
+                    message.reply("The message you replied to is not a funds availability notice. You can also withdraw from any escrow given the escrow ID by using `!withdraw [escrow ID] [address]`" +
+                                  config.signature)
+                    continue
             if (len(m) != 3 and len(m) != 4) :
                 message.reply("Invalid syntax. The correct syntax is `!withdraw [escrow ID] [address]`. Additionally, you may specify your own feerate: `!withdraw [escrow ID] [address] [feerate]`" +
                               config.signature)
@@ -167,6 +193,6 @@ def exists(r: praw.Reddit, username: str) :
     """
     try :
         r.redditor(username).id
-    except NotFound :
+    except prawcore.exceptions.NotFound :
         return False
     return True
